56c4acc3e7012a18b3764c928906ffdd75579524
woogamjaa <woogamjaa@gmail.com>
2025-05-10 00:49:38 +0900

    Feat : onChange  ->  event.target.value 로 상태변경시 console.log 로 감지하기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React from 'react';  
+import { useState } from 'react';
 
-function App() {
-  return (
-   <h1>쇼핑몰 앱 만들어보기</h1>
-  );
+interface ProductType {
+  id: number;
+  name: string;
+  explanation: string;
+  price: number;
 }
 
+function App() {
+  const [products , setProduct ] = useState<ProductType[]>([
+    {
+    id: 102,
+    name: '민영이 사랑해',
+    explanation: '민영이 사랑해 2 ',
+    price: 2000000000
+    },
+  ]);
+  const [name, setName] = useState('');
+  const [explanation, setExplanation] = useState('');
+  const [price, setPrice] = useState(0);
+  
+  console.log(products);
+  return (
+    <>
+    <form 
+      onSubmit={(event) => { 
+        event.preventDefault();
+        console.log("제출");
+        }}>
+      <input onChange={(event) => console.log("상품 이름이 변경되었습니다.", event.target.value)} type="text" placeholder="상품 이름" />
+      <input onChange={(event) => console.log("상품 설명이 변경되었습니다.", event.target.value)} type="text" placeholder="상품 설명" />
+      <input onChange={(event) => console.log("상품 가격이 변경되었습니다.", event.target.value)} type="number" placeholder="상품 가격" />
+      <input type="submit" value="상품 만들기"/> 
+    </form>
+
+    {products.map((product) => (
+      <div key={product.id}>
+        <div>{product.id}</div>
+        <div>{product.name}</div>
+        <div>{product.explanation}</div>
+        <div>{product.price}</div>
+      </div>
+    ))}
+    </>
+  );
+}
 export default App;
