d1f41acd0b31fd36e687264a9bf0d6f93bd67222
woogamjaa <woogamjaa@gmail.com>
2025-05-10 16:26:41 +0900

    init : 상품 상세 페이지 나누기 1. 상품 페이지 생성 2. ts에 export 추가 3. app에 path 넣기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import  { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import {HomePage, ProductPage} from "./pages";
 
 function App() {
     return (
       <Routes>
         <Route index element={<HomePage />} />
+        <Route path="/:products" element={<ProductPage />} />
       </Routes>
   );
 }
 // export default App;
-export default App;
+export default App; 
---
+++ client/src/pages/ProductPage.tsx
@@ -0,0 +1,10 @@
+function ProductPage() {
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <p>This is the product page.</p>
+    </div>
+  );
+}
+
+export default ProductPage;
