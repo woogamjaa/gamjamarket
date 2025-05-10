c0fe8f2f4112e81190fa10cfb5b2840f32a3dd76
woogamjaa <woogamjaa@gmail.com>
2025-05-10 15:57:10 +0900

    init : index.tsx에서 BrowserRouter 컴포넌트로 App 컴포넌트 설치 과정에서 .
    1. react-scripts@5.0.1은 React 19을 공식 지원하지 않기 때문에 버전을 낮출 필요가 있음.
    2. React 18로 다운 npm install react@18 react-dom@18
    3. app, index 수정
    4. homepage.tsx 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,97 +1,13 @@
-import React from 'react';  
-import { useState, useRef } from 'react';
-
-interface ProductType {
-  id: number;
-  name: string;
-  explanation: string;
-  price: number;
-}
-
-interface ProductItemProps {
-  product: ProductType;
-  onDelete: (id: number) => void;
-  onUpdate: (product: ProductType) => void;
-}
-
-const ProductItem = ({ product,onDelete,onUpdate }: ProductItemProps) => {
-  const { id, name, explanation, price } = product;
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [editName, setEditName] = useState(product.name);
-  const [editExplanation, setEditExplanation] = useState(product.explanation);
-  const [editPrice, setEditPrice] = useState(product.price);
-
-  return (
-    <div>
-      <div>{id}</div>
-      <div>{name}</div>
-      <div>{explanation}</div>
-      <div>{price}</div>
-      <button type="button" onClick={()=> onDelete(id)}>삭제</button>
-      <button type="button" onClick={()=> setIsEditMode((prev)=> !prev)}>수정</button>
-      {isEditMode && (
-        <form 
-        onSubmit={(event) => {
-          event.preventDefault();
-          onUpdate({id,
-            name: editName,
-            explanation: editExplanation,
-            price: editPrice,
-          });
-        }}>
-          <input type="text" placeholder='상품 이름' value={editName} onChange={(event)=> setEditName(event.target.value)}/>
-          <input type="text" placeholder='상품 설명' value={editExplanation} onChange={(event)=> setEditExplanation(event.target.value)} />
-          <input type="number" placeholder='상품 가격' value={editPrice} onChange={(event)=>setEditPrice(parseInt(event.target.value))}/>
-          <input type="submit" value="수정하기"/>
-        </form>
-      )}
-    </div>
-  );
-}
+import React from "react";
+import  { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [products , setProducts ] = useState<ProductType[]>([
-  ]);
-  const [name, setName] = useState('');
-  const [explanation, setExplanation] = useState('');
-  const [price, setPrice] = useState(0);
-  const fakeId = useRef(0);
-
-  const handleCreate = (newProduct: Omit<ProductType, 'id'>) => {
-    fakeId.current += 1;
-    setProducts([...products, { ...newProduct, id: fakeId.current }]);
-  }
-  const handleDelete = (id: number) => {
-    setProducts(products.filter((product) => product.id !== id));
-  }
-
-  const handleUpdate = (updateProduct: {
-    id: number;
-    name: string;
-    explanation: string;
-    price: number;
-  }) => {
-    setProducts(products.map((product) => (product.id === updateProduct.id ? updateProduct : product)));
-  }
-
-  console.log(products);
-  return (
-    <>
-    <form 
-      onSubmit={(event) => { 
-        event.preventDefault();
-        handleCreate({name, explanation, price});
-        }}>
-      <input onChange={(event) => setName(event.target.value)} type="text" placeholder="상품 이름" />
-      <input onChange={(event) => setExplanation(event.target.value)} type="text" placeholder="상품 설명" />
-      <input onChange={(event) => setPrice(parseInt(event.target.value, 10))} type="number" placeholder="상품 가격" />
-      <input type="submit" value="상품 만들기"/> 
-    </form>
-
-    {products.map((product) => (
-      <ProductItem key={product.id} product={product} onDelete={handleDelete} onUpdate={handleUpdate} />
-    ))}
-    </>
+    return (
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
   );
 }
+// export default App;
 export default App;
